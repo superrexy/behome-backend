733ae68a90ea6d57ca7deda1ac0a6e4568d69d6e
Bintang Rezeka Ramadani <bintangrezeka@gmail.com>
2023-06-03 07:02:35 +0700

    change env to prod

--- app.js
+++ app.js
@@ -116,8 +116,8 @@ app.use((req, res, next) => {
   });
 });
 
-server.listen(process.env.PORT || 3000, () => {
-  console.log(`Server is running on port ${process.env.PORT || 3000}`);
+server.listen(process.env.APP_PORT || 3000, () => {
+  console.log(`Server is running on port ${process.env.APP_PORT || 3000}`);
 });
 
 module.exports = app;
