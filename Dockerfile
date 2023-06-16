dbf9ea58fc1ab7b4f22f92a03d8fa68d04aeaa7a
Bintang Rezeka Ramadani <66295097+superrexy@users.noreply.github.com>
2023-06-16 15:23:52 +0700

    feat: update node version

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM node:18.12.1-buster-slim
+FROM node:gallium-alpine3.18
 
 # Create app directory
 WORKDIR /usr/src/app
--- app.js
+++ app.js
@@ -11,62 +11,62 @@ const prisma = new PrismaClient();
 const app = express();
 const server = createServer(app);
 const io = new Server(server, {
-  cors: {
-    origin: "*",
-  },
+    cors: {
+        origin: "*",
+    },
 });
 
 io.on("connection", (socket) => {
-  socket.on("chats", async (data) => {
-    socket.join(`chats-${data.user_id}`);
-  });
+    socket.on("chats", async (data) => {
+        socket.join(`chats-${data.user_id}`);
+    });
 
-  socket.on("chat", async (data) => {
-    socket.join(`chat-${data.chat_id}`);
-  });
+    socket.on("chat", async (data) => {
+        socket.join(`chat-${data.chat_id}`);
+    });
 });
 
 schedule.scheduleJob("*/1 * * * *", async () => {
-  const chats = await prisma.chat_rooms.findMany({
-    where: {
-      AND: [
-        {
-          is_closed: false,
-          is_finished: false,
-        },
-      ],
-    },
-    include: {
-      schedule: {},
-    },
-  });
-
-  chats.forEach(async (chat) => {
-    // close chat if message 1 hour closed time
-    const now = new Date();
-    const expired = new Date(chat.consultation_day).setHours(
-      chat.schedule.time.split(":")[0],
-      chat.schedule.time.split(":")[1],
-      0,
-      0
-    );
-    const diff = now.getTime() - expired;
-    const diffHours = Math.floor(diff / 1000 / 60 / 60);
-
-    if (diffHours >= 1) {
-      await prisma.chat_rooms.update({
+    const chats = await prisma.chat_rooms.findMany({
         where: {
-          id: chat.id,
+            AND: [
+                {
+                    is_closed: false,
+                    is_finished: false,
+                },
+            ],
         },
-        data: {
-          is_closed: true,
+        include: {
+            schedule: {},
         },
-      });
+    });
 
-      io.to(`chats-${chat.user_id}`).emit("chat-closed", true);
-      io.to(`chat-${chat.id}`).emit("chat-closed", true);
-    }
-  });
+    chats.forEach(async (chat) => {
+        // close chat if message 1 hour closed time
+        const now = new Date();
+        const expired = new Date(chat.consultation_day).setHours(
+            chat.schedule.time.split(":")[0],
+            chat.schedule.time.split(":")[1],
+            0,
+            0
+        );
+        const diff = now.getTime() - expired;
+        const diffHours = Math.floor(diff / 1000 / 60 / 60);
+
+        if (diffHours >= 1) {
+            await prisma.chat_rooms.update({
+                where: {
+                    id: chat.id,
+                },
+                data: {
+                    is_closed: true,
+                },
+            });
+
+            io.to(`chats-${chat.user_id}`).emit("chat-closed", true);
+            io.to(`chat-${chat.id}`).emit("chat-closed", true);
+        }
+    });
 });
 
 const authRouter = require("./app/api/auth/auth.router");
@@ -84,25 +84,25 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use("/public", express.static(path.join(__dirname, "public")));
 app.use((req, res, next) => {
-  req.io = io;
-  return next();
+    req.io = io;
+    return next();
 });
 
 const swaggerUi = require("swagger-ui-express");
 const swaggerFile = require("./swagger_output.json");
 app.use(
-  `${URL_PREFIX_API_V1}/docs`,
-  swaggerUi.serve,
-  swaggerUi.setup(swaggerFile)
+    `${URL_PREFIX_API_V1}/docs`,
+    swaggerUi.serve,
+    swaggerUi.setup(swaggerFile)
 );
 
 app.get(`${URL_PREFIX_API_V1}/docs-json`, (req, res) => {
-  return res.status(200).send(swaggerFile);
+    return res.status(200).send(swaggerFile);
 });
 
 // Index Route
 app.get("/", (req, res) => {
-  return res.send("Behome Backend");
+    return res.send("Behome Backend");
 });
 
 // API Routes
@@ -114,14 +114,12 @@ app.use(`${URL_PREFIX_API_V1}`, orderRouter);
 app.use(`${URL_PREFIX_API_V1}`, chatsRouter);
 
 app.use((req, res, next) => {
-  return res.status(404).json({
-    status: false,
-    message: "PAGE_NOT_FOUND",
-  });
+    return res.status(404).json({
+        status: false,
+        message: "PAGE_NOT_FOUND",
+    });
 });
 
 server.listen(process.env.APP_PORT || 3000, () => {
-  console.log(`Server is running on port ${process.env.APP_PORT || 3000}`);
+    console.log(`Server is running on port ${process.env.APP_PORT || 3000}`);
 });
-
-module.exports = app;
